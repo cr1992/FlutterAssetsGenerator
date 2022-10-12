c6eed0c63133dc26a554f55fc2a8dd5da6fdd645
cairui <wb.cairui01@mesg.corp.netease.com>
2022-10-12 12:01:52 +0800

    修复svg预览没有正常展示的问题

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+<!-- Keep a Changelog guide -> https://keepachangelog.com -->
+
+# FlutterAssetsGenerator Changelog
+
+## [2.1.1]
+### Fixed
+- Svg preview not displaying
+## [2.1.0]
+### Fixed
+- Bug fix
+### Added
+- Filename split supports config.
--- src/main/java/com/crzsc/plugin/provider/AssetsLineMarkerProvider.kt
+++ src/main/java/com/crzsc/plugin/provider/AssetsLineMarkerProvider.kt
@@ -1,5 +1,6 @@
 package com.crzsc.plugin.provider
 
+import com.crzsc.plugin.utils.FileHelper
 import com.crzsc.plugin.utils.PluginUtils.openFile
 import com.crzsc.plugin.utils.isImageExtension
 import com.crzsc.plugin.utils.isSvgExtension
@@ -28,8 +29,16 @@ class AssetsLineMarkerProvider : LineMarkerProvider {
      * Fixme 大图性能优化，添加缓存策略
      */
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
-        if (element.containingFile.name.contains("Assets.dart", true)
-                && (element as? LeafPsiElement?)?.elementType?.toString() == "REGULAR_STRING_PART"
+        val filenameCorrect = element.containingFile.name.equals(
+            FileHelper.getGeneratedFile(
+                element.project
+            ).name, true
+        ) || element.containingFile.name.equals(
+            "assets.dart", true
+        )
+        val elementType = (element as? LeafPsiElement?)?.elementType?.toString()
+        if (filenameCorrect
+            && elementType == "REGULAR_STRING_PART"
         ) {
             return showMakeByType(element)
         }
@@ -39,28 +48,44 @@ class AssetsLineMarkerProvider : LineMarkerProvider {
     private fun showMakeByType(element: PsiElement): LineMarkerInfo<*>? {
         val assetsPath = element.text
         val anchor = PsiTreeUtil.getDeepestFirst(element)
-        val filePath = element.project.basePath + "/" + element.text
-        val vFile = LocalFileSystem.getInstance().findFileByPath(filePath) ?: return null
-        return when {
-            assetsPath.isSvgExtension -> showSvgMark(element, anchor, vFile)
-            assetsPath.isImageExtension -> showImageMark(element, anchor, vFile)
-            else -> null
+        // 先用默认的path找文件
+        var filePath = element.project.basePath + "/" + element.text
+        var vFile = LocalFileSystem.getInstance().findFileByPath(filePath)
+        if (vFile == null) {
+            // 如果没找到，尝试根据当前文件向上查找
+            var file = element.containingFile.viewProvider.virtualFile
+            while (file.name != "lib" && file.parent != null) {
+                file = file.parent
+            }
+            filePath = file.parent.path + "/" + element.text
+            vFile = LocalFileSystem.getInstance().findFileByPath(filePath)
         }
+//        println("showMakeByType assetsPath $assetsPath")
+        if (vFile != null) {
+            return when {
+                assetsPath.isSvgExtension -> showSvgMark(element, anchor, vFile)
+                assetsPath.isImageExtension -> showImageMark(element, anchor, vFile)
+                else -> null
+            }
+        }
+        return null
     }
 
 
-    private fun showSvgMark(element: PsiElement, anchor: PsiElement,
-                            vFile: VirtualFile): LineMarkerInfo<*>? {
+    private fun showSvgMark(
+        element: PsiElement, anchor: PsiElement,
+        vFile: VirtualFile
+    ): LineMarkerInfo<*> {
         val icon: Icon =
-                ImageIcon(
-                        SVGLoader.load(
-                                null,
-                                vFile.inputStream,
-                                ScaleContext.createIdentity(),
-                                16.0,
-                                16.0
-                        )
+            ImageIcon(
+                SVGLoader.load(
+                    null,
+                    vFile.inputStream,
+                    ScaleContext.createIdentity(),
+                    16.0,
+                    16.0
                 )
+            )
         return LineMarkerInfo(anchor, anchor.textRange, icon, {
             //悬停，会多次调用
             return@LineMarkerInfo ""
@@ -69,16 +94,19 @@ class AssetsLineMarkerProvider : LineMarkerProvider {
     }
 
 
-    private fun showImageMark(element: PsiElement, anchor: PsiElement,
-                              vFile: VirtualFile): LineMarkerInfo<*>? {
+    private fun showImageMark(
+        element: PsiElement, anchor: PsiElement,
+        vFile: VirtualFile
+    ): LineMarkerInfo<*> {
         val icon = IconUtil.getIcon(vFile, Iconable.ICON_FLAG_VISIBILITY, element.project)
         //其他文件展示文件格式
         return LineMarkerInfo(
-                anchor, anchor.textRange,
-                icon, {
-            //悬停，会多次调用
-            return@LineMarkerInfo ""
-        }, { _, _ -> element.openFile(vFile) }, GutterIconRenderer.Alignment.LEFT)
+            anchor, anchor.textRange,
+            icon, {
+                //悬停，会多次调用
+                return@LineMarkerInfo ""
+            }, { _, _ -> element.openFile(vFile) }, GutterIconRenderer.Alignment.LEFT
+        )
 
     }
 
