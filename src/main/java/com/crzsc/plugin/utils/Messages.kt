ee6da84f8284cf057e35263013cbd3768c573233
cr1992 <cr1992@qq.com>
2026-02-04 08:58:53 +0800

    解决 message 报错

--- src/main/java/com/crzsc/plugin/utils/Messages.kt
+++ src/main/java/com/crzsc/plugin/utils/Messages.kt
@@ -4,7 +4,6 @@ import java.util.*
 
 
 fun message(key: String): String {
-    val l = if (Locale.getDefault().language.equals("us") or Locale.getDefault().language.equals("en")) "" else "_" + Locale.getDefault().language
-    return ResourceBundle.getBundle("messages.MessagesBundle$l").getString(key)
+    return ResourceBundle.getBundle("messages.MessagesBundle").getString(key)
 }
 
